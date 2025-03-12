a10213da5d88415e05d17955f52715c38fc0a4c0
Ankit Kumar <122913003+anris18@users.noreply.github.com>
2025-03-13 00:05:42 +0530

    Update infosys.py

--- infosys.py
+++ infosys.py
@@ -7,10 +7,20 @@ import re
 import os
 from difflib import get_close_matches
 import streamlit as st
+import kagglehub
+path = kagglehub.dataset_download("niksaurabh/doctors-speciality")
 
+print("Path to dataset files:", path)
+
+# Load the dataset into a Pandas DataFrame
+csv_files = [file for file in os.listdir(path) if file.endswith('.csv')]
+
+if csv_files:
+    file_path = os.path.join(path, csv_files[0])
+    df = pd.read_csv(file_path)
 
 # Dictionary to store doctors by specialty
-
+doctors_by_specialty = df.groupby('speciality')['Doctor\'s Name'].apply(list).to_dict()
 
 # Mapping of symptoms to specialties
 symptom_to_specialty = {
@@ -514,6 +524,8 @@ import time
 
 import streamlit as st
 
+import streamlit as st
+
 def main():
     st.markdown(
         """
@@ -599,7 +611,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # Run the Streamlit app using the `streamlit run` command
-    import subprocess
-    import sys
-    subprocess.run([sys.executable, "-m", "streamlit", "run", __file__] + sys.argv[1:])
+  
